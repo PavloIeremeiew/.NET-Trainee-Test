3f1a6bbdb005f85b5eeddccc494c86685f51e92c
Pavlo Ieremeiew <peremuv3@gmail.com>
2024-09-11 17:07:05 +0300

    init  mvc project

---
+++ NET-Trainee-Test-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NET_Trainee_Test_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
